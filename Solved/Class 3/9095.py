0237a57a45a14a86fd5ab8346fc1f7c2870b0871
sunghyunzzzzang <tjdgus037@gmail.com>
2021-11-02 16:11:21 +0900

    BOJ solving 9095 // 2021-11-02 DP

---
+++ Solved/Class 3/9095.py
@@ -0,0 +1,16 @@
+# 9095. 1, 2, 3 더하기
+# DP
+
+TC = int(input())
+for tc in range(1, TC + 1):
+    n = int(input())
+    arr = [0] * 12
+    arr[0] = 0
+    arr[1] = 1
+    arr[2] = 2
+    arr[3] = 4
+
+    for i in range(4, n+1):
+        arr[i] = arr[i-1] + arr[i-2] + arr[i-3]
+
+    print(arr[n])
