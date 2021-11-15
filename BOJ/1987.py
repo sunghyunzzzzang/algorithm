b055d1b7f56831317c907f1d5ee8413381c9ab96
sunghyunzzzzang <tjdgus037@gmail.com>
2021-11-15 21:39:46 +0900

    BOJ solved 1987 // 2021-11-15

--- BOJ/1987.py
+++ BOJ/1987.py
@@ -1,34 +1,5 @@
 # 1987. 알파벳
 
-dict = {
-    'A' : 0,
-    'B' : 0,
-    'C' : 0,
-    'D' : 0,
-    'E' : 0,
-    'F' : 0,
-    'G' : 0,
-    'H' : 0,
-    'I' : 0,
-    'J' : 0,
-    'K' : 0,
-    'L' : 0,
-    'M' : 0,
-    'N' : 0,
-    'O' : 0,
-    'P' : 0,
-    'Q' : 0,
-    'R' : 0,
-    'S' : 0,
-    'T' : 0,
-    'U' : 0,
-    'V' : 0,
-    'W' : 0,
-    'X' : 0,
-    'Y' : 0,
-    'Z' : 0,
-}
-
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
@@ -49,12 +20,13 @@ def dfs(x, y, result):
         if visited[nx][ny] != 0:
             continue
         # 새로운 알파벳이면
-        if dict[maps[nx][ny]] == 0:
-            dict[maps[nx][ny]] = 1
+        index = ord(maps[nx][ny]) - ord('A')
+        if dict[index] == 0:
+            dict[index] = 1
             visited[nx][ny] = 1
             dfs(nx, ny, result + 1)
             visited[nx][ny] = 0
-            dict[maps[nx][ny]] = 0
+            dict[index] = 0
 
 N, M = map(int, input().split())
 maps = []
@@ -62,10 +34,11 @@ for i in range(N):
     maps.append(input())
 
 visited = [[0] * M for i in range(N)]
+dict = [0] * 26
 answer = 0
 
 visited[0][0] = 1
-dict[maps[0][0]] = 1
+dict[ord(maps[0][0]) - ord('A')] = 1
 dfs(0, 0, 1)
 
 print(answer)
